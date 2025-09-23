2d5c501d7af662c02c448dcdacbe386198db613a
mocs <moco59@gmail.com>
2025-09-23 14:47:46 -0300

    Added support python 3.11.

--- celerybeatredis/task.py
+++ celerybeatredis/task.py
@@ -136,7 +136,8 @@ class PeriodicTask(object):
                 sleep(4)
         for task_key in tasks:
             try:
-                dct = json.loads(bytes_to_str(rdb.get(task_key)), cls=DateTimeDecoder, encoding=default_encoding)
+                dct = json.loads(bytes_to_str(rdb.get(task_key)), cls=DateTimeDecoder)
+                #dct = json.loads(bytes_to_str(rdb.get(task_key)), cls=DateTimeDecoder, encoding=default_encoding)
                 # task name should always correspond to the key in redis to avoid
                 # issues arising when saving keys - we want to add information to
                 # the current key, not create a new key
